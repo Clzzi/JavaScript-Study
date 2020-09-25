9e8c4c66d79cc3a367e441755d458e40fcf8de76
Clzzi <sonmin1407@dgsw.hs.kr>
2020-09-25 20:38:32 +0900

    (7-3)지뢰찾기 - 물음표 변환 , 초기화 생성

--- LandMine.js
+++ LandMine.js
@@ -1,11 +1,11 @@
 var tbody = document.querySelector('#table tbody');
 const dataset = [];
-document.querySelector("#exec").addEventListener('click',function(){
+document.querySelector('#exec').addEventListener('click',function(){
+    tbody.innerHTML = ''; //내부 초기화
     const hor = parseInt(document.querySelector('#hor').value);
     const ver = parseInt(document.querySelector('#ver').value);
     const mine = parseInt(document.querySelector('#mine').value);
     //parseInt => 숫자나 문자열을 정수형으로 바꿔줌
-    console.log(hor, ver, mine);
     const mineset = (hor*ver) - (mine);
     const candidate = Array(hor * ver)
         .fill()
@@ -22,7 +22,6 @@ document.querySelector("#exec").addEventListener('click',function(){
         // 정렬된 배열에서 랜덤수를 새로운 배열에 저장해 정렬된 1 부터 n까지의 수를 
         // 랜덤적으로 shuffle에 넣어줌  
 
-    const tbody = document.querySelector('#table tbody');
     for(let i = 0;i < ver ; i++)    
     {
         const arr = [];
@@ -38,9 +37,18 @@ document.querySelector("#exec").addEventListener('click',function(){
                 const Mtbody = e.currentTarget.parentNode.parentNode;
                 const can = Array.prototype.indexOf.call(Mtr.children, e.currentTarget);
                 const jul = Array.prototype.indexOf.call(Mtbody.children, Mtr);
-                console.log(Mtr,Mtbody,can, jul);
-                e.currentTarget.textContent = "!";
-                dataset[jul][can] = '!';
+                if(e.currentTarget.textContent === '' || e.currentTarget.textContent === 'X'){
+                    e.currentTarget.textContent = '!';
+                } else if (e.currentTarget.textContent === '!'){
+                    e.currentTarget.textContent = '?';
+                }
+                else if(e.currentTarget.textContent === '?'){
+                    if(dataset[jul][can] === 1){
+                        e.currentTarget.textContent = '';
+                    } else if(dataset[jul][can] === 'X'){
+                        e.currentTarget.textContent = 'X';
+                    }
+                }
             });
             tr.appendChild(td);
         }
@@ -55,5 +63,4 @@ document.querySelector("#exec").addEventListener('click',function(){
         dataset[height][width] = 'X';
         // 가로 세로 지정해주고 셔플에 넣어진 값을 차례대로 tbody의 자손인 즉, tr에 지뢰를 넣어줌
     }
-    console.log(dataset);
 });
